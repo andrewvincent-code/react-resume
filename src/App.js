bfd06ef6e7c33782b9d0c4cc1650d02529c7d58b
Andrew Vincent <andrewvincent@Andrews-MBP-2.attlocal.net>
2020-06-01 08:12:35 -0700

    updated bug

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ export default function ControlledExpansionPanels() {
             </Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <General/> //Calling a component that holds all the content of General Information
+            <General/> {/*Calling a component that holds all the content of General Information*/}
           </ExpansionPanelDetails>
         </ExpansionPanel>
 
